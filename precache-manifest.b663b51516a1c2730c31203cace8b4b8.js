a06f69f7e3664f206ad7ca05bb49784953886790
BOUZAABIAM <mohamedyoussefbouzaabia@gmail.com>
2019-11-07 12:50:31 +0100

    Updates

---
+++ precache-manifest.b663b51516a1c2730c31203cace8b4b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9331a61a63b5c7ded21c836dea00e21",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "54f32938b1f01d819fbc",
+    "url": "/portfolio/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "54f32938b1f01d819fbc",
+    "url": "/portfolio/static/js/2.f9719fd0.chunk.js"
+  },
+  {
+    "revision": "3b776e9d900732f2a82b",
+    "url": "/portfolio/static/js/main.e7973487.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e6e1053fb2e67410d6b5de2aeddceb65",
+    "url": "/portfolio/static/media/cs440.e6e1053f.png"
+  },
+  {
+    "revision": "bb34f7ecb5a3f9748b009eea56d102af",
+    "url": "/portfolio/static/media/lotrScreenshot.bb34f7ec.png"
+  },
+  {
+    "revision": "6ebe9a1bc12afc218ab214fbcf11482c",
+    "url": "/portfolio/static/media/myb_picture.6ebe9a1b.png"
+  },
+  {
+    "revision": "f3555398ea17face56e5756c44f2a049",
+    "url": "/portfolio/static/media/procaryotaScreenshot.f3555398.png"
+  },
+  {
+    "revision": "566af88072294cdcafcb206c39dd34f3",
+    "url": "/portfolio/static/media/qulkanScreenshot.566af880.png"
+  },
+  {
+    "revision": "9f1eb595f49c6707aa11cbee77b1181b",
+    "url": "/portfolio/static/media/resume.9f1eb595.pdf"
+  },
+  {
+    "revision": "882077d69813e0ee3e6835c36a89ba05",
+    "url": "/portfolio/static/media/unity_logo.882077d6.png"
+  }
+]);
